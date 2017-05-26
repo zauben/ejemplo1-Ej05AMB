b45cc21fecbda4536f6da4bef1938162668102ef
virtual <virtual@10.0.2.15>
2017-05-26 18:21:40 -0300

    primer commit

---
+++ src/Ejemplo1/Alien.java
@@ -0,0 +1,12 @@
+package Ejemplo1;
+
+public class Alien {
+String nombre;
+static int sigNumeroDeAlien;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejemplo..");
+	}
+
+}
